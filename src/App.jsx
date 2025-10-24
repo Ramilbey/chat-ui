293ab89e64b00e628709d541d15fa3734b871ec3
Ramilbey <ramilbey505711503@gmail.com>
2025-10-24 15:33:15 +0800

    make icons interactive

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Sider from "./components/Sider"
-import ChatUserList from "./components/ChatUserList"
-import Chat from "./components/Chat"
+// import ChatUserList from "./components/ChatUserList"
+// import Chat from "./components/Chat"
 import "./App.css"
 
 function App() {
   return (
     <div className="app-container">
       <Sider />
-      <ChatUserList />
-      <Chat />
+      {/* <ChatUserList />
+      <Chat /> */}
       
 
     </div>
--- src/components/Sider.jsx
+++ src/components/Sider.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaUserFriends, FaComments, FaUsers, FaCog } from "react-icons/fa";
+
 import "./Sider.css";
 
 function Sider() {
-    return (
-        <div className="sider">
-            Sidebar
-        </div>
-    )
+  const [active, setActive] = useState("users");
+  return (
+    <div className="sider">
+      <div
+        className={`icon &{active == "users ? "active" : ""}`}
+        onClick={() => setActive("users")}
+      >
+        <FaUserFriends size={24} />
+      </div>
+
+      <div
+        className={`icon &{active == "chats ? "active" : ""}`}
+        onClick={() => setActive("chats")}
+      >
+        <FaComments size={24} />
+      </div>
+
+      <div
+        className={`icon &{active == "users ? "groups" : ""}`}
+        onClick={() => setActive("groups")}
+      >
+        <FaUsers size={24} />
+      </div>
+
+      <div
+        className={`icon &{active == "settings ? "active" : ""}`}
+        onClick={() => setActive("settings")}
+      >
+        <FaCog size={24} />
+      </div>
+
+      
+    </div>
+  );
 }
 export default Sider;
