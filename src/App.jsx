dfb40bfbb9cb0f78e75dbc777fc2f94c93574f48
Ramilbey <ramilbey505711503@gmail.com>
2025-10-24 17:22:31 +0800

    change the lay out

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import Sider from "./components/Sider";
 import ChatUserList from "./components/ChatUserList"
+import Group from "./components/Groups";
 // import Chat from "./components/Chat"
 import "./App.css";
 
 function App() {
   return (
     <div className="app-container">
-      <Sider />
-      <ChatUserList />
-      {/* <Chat />  */}
-    </div>
+  <div className="sider">
+    <Sider />
+  </div>
+
+  <div className="left-panel">
+    <ChatUserList />
+    <div className="spacer"></div>
+    <Group />
+  </div>
+
+  {/* <div className="chat-area">
+    <Chat />
+  </div> */}
+
+  {/* <div className="user-details">
+    <UserDetails />
+  </div> */}
+</div>
+
+   
   );
 }
 
---
+++ src/components/Groups.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Group.css";
+
+function Group() {
+    return (
+        <div>
+            hello from group
+        </div>
+    )
+}
+export default Group;
